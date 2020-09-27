f636aa9441d83cc97a4881d1fff03f3fc347ade7
Dominik <dominik.niec@altconnect.pl>
2020-09-27 15:52:39 +0200

    Create simple implementation of add method.

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -3,6 +3,13 @@ package com.example;
 public class StringCalculator {
 
     int add(String numbers) {
-       return 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        } else if (numbers.length() == 1) {
+            return Integer.parseInt(numbers);
+        } else {
+            String[] numberArray = numbers.split(",");
+            return Integer.parseInt(numberArray[0]) + Integer.parseInt(numberArray[1]);
+        }
     }
 }
