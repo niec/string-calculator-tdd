015015eae9167933ae8cef8b12fefe13e016ab33
Dominik <dominik.niec@altconnect.pl>
2020-09-27 17:50:22 +0200

    Implement setting delimiter in first line.

---
+++ src/main/java/com/example/NumberInput.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class NumberInput {
+    private String numbers;
+
+    public NumberInput(String numbers) {
+        this.numbers = numbers;
+    }
+
+    public void setNumbers(String numbers) {
+        this.numbers = numbers;
+    }
+
+    public String getNumbers() {
+        return numbers;
+    }
+}
--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -1,21 +1,45 @@
 package com.example;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private final Pattern customDelimiterPattern = Pattern.compile("\\/\\/.+\\\n");
+    private Matcher matcher;
+    private static final String DEFAULT_DELIMITER = ",";
 
     int add(String numbers) {
-        if (numbers.isEmpty()) {
+        var input = new NumberInput(numbers);
+
+        if (input.getNumbers().isEmpty()) {
             return 0;
-        } else if (numbers.length() == 1) {
-            return Integer.parseInt(numbers);
+        } else if (input.getNumbers().length() == 1) {
+            return Integer.parseInt(input.getNumbers());
         } else {
+            final String delimiter = getDelimiter(input);
+
             return Arrays.stream(
-                    numbers.replaceAll("\n", ",")
-                            .split(","))
+                    input.getNumbers()
+                            .replaceAll("\n", ",")
+                            .split(delimiter))
                     .map(Integer::parseInt)
                     .reduce((n1, n2) -> n1 + n2)
                     .get();
         }
     }
+
+    private String getDelimiter(NumberInput input) {
+        this.matcher = customDelimiterPattern.matcher(input.getNumbers());
+        if (matcher.find()) {
+            input.setNumbers(input.getNumbers().substring(input.getNumbers().indexOf('\n') + 1)); //remove first line
+            return obtainSingleDelimiter(matcher.group(0));
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    private String obtainSingleDelimiter(String firstLine) {
+        return firstLine.substring(firstLine.indexOf("//") + 2, firstLine.indexOf("\n"));
+    }
+
 }
