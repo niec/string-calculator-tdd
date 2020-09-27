e4d0fcae853ee00668d0132ddd8ab07c1f826bc9
Dominik <dominik.niec@altconnect.pl>
2020-09-27 15:48:14 +0200

    Create tests for empty, single and double number string.

---
+++ src/main/java/com/example/StringCalculator.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class StringCalculator {
+
+    int add(String numbers) {
+       return 0;
+    }
+}
---
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+    private static StringCalculator calculator;
+
+    @BeforeClass
+    public static void setup() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    public void shouldReturn0ForAnEmptyString() {
+        int result = calculator.add("");
+
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void shouldReturnNumberForASingleNumberString() {
+        int result = calculator.add("1");
+
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldReturnNumbersSumForADoubleNumberString() {
+        int result = calculator.add("1,2");
+
+        Assert.assertEquals(3, result);
+    }
+
+}
