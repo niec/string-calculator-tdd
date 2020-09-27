c2fb4bebe73a3202ae77cf535799f9b103401366
Dominik <dominik.niec@altconnect.pl>
2020-09-27 16:16:51 +0200

    Add test case for newline character as delimeter.

--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -40,4 +40,10 @@ public class StringCalculatorTest {
         Assert.assertEquals(306, result);
     }
 
+    @Test
+    public void shouldSupportNewLineCharacterAsDelimeter() {
+        int result = calculator.add("1\n2,3");
+
+        Assert.assertEquals(6, result);
+    }
 }
